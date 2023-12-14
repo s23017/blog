c81f42a9494a069209c08e5b80fc1acbf4650e05
s23017 <s23017@std.it-college.ac.jp>
2023-12-14 06:59:31 +0000

    p53-p59
    ページを増やすまで

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,6 @@
-import Header from 'components/header.js'
 import Hero from 'components/hero.js'
-import Footer from 'components/footer.js'
 const Home = () => {
-  return (
-    <>
-      <Header />
-      <main>
-        <Hero />
-      </main>
-      <Footer />
-    </>
-  )
+  return <Hero />
 }
 
 export default Home
