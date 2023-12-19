3e85c8c044b144cd4018c5c5837bfdc1df7589f5
s23017 <s23017@std.it-college.ac.jp>
2023-12-19 06:47:44 +0000

    chapter3
    チャプター３完了

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Nav from '../components/nav.js'
 // const Header = () => <header>HEADER</header>
 const Header = () => (
   <header>
-    <Logo />
+    <Logo boxOn />
     <Nav />
   </header>
 )
