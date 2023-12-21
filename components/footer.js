99b0625f84bb52ddf9d6a41a443051ab0dc1b4f4
s23017 <s23017@std.it-college.ac.jp>
2023-12-21 08:32:33 +0000

    5.9 finish
    5.9完了

--- components/footer.js
+++ components/footer.js
@@ -1,12 +1,13 @@
 import Container from 'components/container'
 import Logo from 'components/logo'
+import Social from 'components/social'
 import styles from 'styles/footer.module.css'
 const Footer = () => (
   <footer className={styles.wrapper}>
     <Container>
       <div className={styles.flexContainer}>
         <Logo />
-        [ソーシャル]
+        <Social />
       </div>
     </Container>
   </footer>
