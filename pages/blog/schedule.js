7a43ceb0010585810771068715be20e961a77507
s23017 <s23017@std.it-college.ac.jp>
2024-01-09 09:45:28 +0000

    76 totyu
    7.6途中

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,23 +1,27 @@
-import { client } from 'lib/api'
+import { getPostBySlug } from 'lib/api'
+import Container from 'components/container'
 
-const Schedule = () => {
-  return <h1>記事のタイトル</h1>
+const Schedule = ({ title, publish, content, eyecatch, categories }) => {
+  return (
+    <Container>
+      <h1>{title}</h1>
+    </Container>
+  )
 }
-export default Schedule
 
 const getStaticProps = async () => {
-  const resPromise = client.get({
-    endpoint: 'blogs'
-  })
-
-  try {
-    const res = await resPromise
-    console.log(res)
-  } catch (err) {
-    console.log(err)
-  }
+  const slug = 'music'
+  const post = await getPostBySlug(slug)
   return {
-    props: {}
+    props: {
+      title: post.title,
+      publish: post.publishDate,
+      content: post.content,
+      eyecatch: post.eyecatch,
+      categories: post.categories
+    }
   }
 }
+
 export { getStaticProps }
+export default Schedule
