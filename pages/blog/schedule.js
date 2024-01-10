f7e9495662c56ad19a54e2dcddaccd9532e58b00
s23017 <s23017@std.it-college.ac.jp>
2024-01-10 06:44:55 +0000

    8.1 finish
    ８．１完了

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -1,10 +1,13 @@
 import { getPostBySlug } from 'lib/api'
 import Container from 'components/container'
+import PostHeader from 'components/post-header'
 
 const Schedule = ({ title, publish, content, eyecatch, categories }) => {
   return (
     <Container>
-      <h1>{title}</h1>
+      <article>
+        <PostHeader title={title} subtitle='Blog Article' publish={publish} />
+      </article>
     </Container>
   )
 }
