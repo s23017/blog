10decdcf5184992f3d3168f6905a7e40b07674b0
s23017 <s23017@std.it-college.ac.jp>
2024-01-11 06:44:10 +0000

    9.3 finish
    ９．３完了

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -16,7 +16,7 @@ import { getPlaiceholder } from 'plaiceholder'
 
 import { eyecatchLocal } from 'lib/constants'
 
-const Schedule = ({
+const Post = ({
   title,
   publish,
   content,
@@ -65,9 +65,14 @@ const Schedule = ({
     </Container>
   )
 }
-
-const getStaticProps = async () => {
-  const slug = 'micro'
+const getStaticPaths = async () => {
+  return {
+    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    fallback: false
+  }
+}
+const getStaticProps = async context => {
+  const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
 
@@ -90,5 +95,6 @@ const getStaticProps = async () => {
   }
 }
 
+export { getStaticPaths }
 export { getStaticProps }
-export default Schedule
+export default Post
