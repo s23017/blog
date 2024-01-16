a3fcc4b5ea4fd751b57e217d3fe4ba37c54ec9c4
s23017 <s23017@std.it-college.ac.jp>
2024-01-16 08:52:02 +0000

    9.6 finish
    9.6 kanryou

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,6 +3,10 @@ import Meta from 'components/meta'
 import Container from 'components/container'
 import Hero from 'components/hero'
 import Posts from 'components/posts'
+import { getPlaiceholder } from 'plaiceholder'
+
+// ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from 'lib/constants'
 
 const Blog = ({ posts }) => {
   return (
@@ -13,8 +17,17 @@ const Blog = ({ posts }) => {
     </Container>
   )
 }
+
 const getStaticProps = async () => {
   const posts = await getAllPosts()
+
+  for (const post of posts) {
+    if (!post.hasOwnProperty('eyecatch')) {
+      post.eyecatch = eyecatchLocal
+    }
+    const { base64 } = await getPlaiceholder(post.eyecatch.url)
+    post.eyecatch.blurDataURL = base64
+  }
 
   return {
     props: {
